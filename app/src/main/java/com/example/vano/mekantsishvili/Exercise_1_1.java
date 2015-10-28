563c6e8b41ddafdc40fc71560bb14233b9700ea7
vano <v.mekantsishvili@gmail.com>
2015-10-28 20:19:14 +0400

    FlashLight

--- app/src/main/java/com/example/vano/mekantsishvili/Exercise_1_1.java
+++ app/src/main/java/com/example/vano/mekantsishvili/Exercise_1_1.java
@@ -2,9 +2,11 @@ package com.example.vano.mekantsishvili;
 
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 
 import com.example.vano.geolabb.R;
@@ -12,6 +14,8 @@ import com.example.vano.geolabb.R;
 public class Exercise_1_1 extends AppCompatActivity implements View.OnClickListener {
 
     private LinearLayout linLay;
+    private static String COLOR_VALUE;
+    int mColor;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,29 +30,47 @@ public class Exercise_1_1 extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.green).setOnClickListener(this);
         findViewById(R.id.yellow).setOnClickListener(this);
 
-
+        if (savedInstanceState != null){
+            mColor = savedInstanceState.getInt(COLOR_VALUE);
+            linLay.setBackgroundColor(mColor);
+        }
 
     }
-
     public void onClick(View v) {
         int color;
         switch (v.getId()) {
             case R.id.blue:
-                linLay.setBackgroundColor(0xff0000ff);
-                color = 0xff0000ff;
+                //linLay.setBackgroundColor(0xff0000ff);
+                mColor = Color.parseColor("#0000ff");
+                linLay.setBackgroundColor(mColor);
+               // color = 0xff0000ff;
                 break;
             case R.id.green:
-                linLay.setBackgroundColor(0xff00ff00);
+                mColor = Color.parseColor("#00ff00");
+                linLay.setBackgroundColor(mColor);
+             //   linLay.setBackgroundColor(0xff00ff00);
                 break;
             case R.id.red:
-                linLay.setBackgroundColor(0xffff0000);
+                mColor = Color.parseColor("#ff0000");
+                linLay.setBackgroundColor(mColor);
                 break;
             case R.id.yellow:
-                linLay.setBackgroundColor(0xffffff00);
+                mColor = Color.parseColor("#ffff00");
+                linLay.setBackgroundColor(mColor);
                 break;
             case R.id.white:
-                linLay.setBackgroundColor(0xffffffff);
+                mColor = Color.parseColor("#ffffff");
+                linLay.setBackgroundColor(mColor);
                 break;
         }
     }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        // TODO Auto-generated method stub
+        super.onSaveInstanceState(outState);
+        outState.putInt(COLOR_VALUE, mColor);
+    }
 }
+
+
+
